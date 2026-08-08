e6b6c4250f88293bb1228f227b75b10df14cf1bb
ronir0613 <ronirapaka13@gmial.com>
2026-08-08 10:53:44 +0530

    changes for adsense

--- regular-meridian/astro.config.mjs
+++ regular-meridian/astro.config.mjs
@@ -10,6 +10,10 @@ export default defineConfig({
     react(),
     sitemap()
   ],
+  redirects: {
+    '/about-us': '/about',
+    '/contact-us': '/contact'
+  },
   vite: {
     plugins: [tailwindcss()],
   },
